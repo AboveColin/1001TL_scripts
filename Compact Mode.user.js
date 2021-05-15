c13a6e40f946b8353a3da239c8b8d415d3894838
AboveColin <colindv2000@gmail.com>
2021-05-15 14:35:07 +0200

    Update Compact Mode.user.js

--- Compact Mode.user.js
+++ Compact Mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Compact Mode
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.3
 // @description  Compact Mode for tracklists on 1001tracklists
 // @author       AboveColin
 // @match        https://www.1001tracklists.com/tracklist/*
@@ -15,4 +15,8 @@
     $(".bItm").css("margin-top", "0px");
     $(".mt5").css("margin-top", "0px");
     $(".bItm").css("grid-template-columns", "0px 40px auto");
+    $(".bCont").css("display", "block");
+    var MediaRow = document.getElementsByClassName("iRow grow mediaRow");
+    var Row = document.getElementsByClassName("bCont tl");
+    $('.mediaRow').each(function(i, obj) {Row[i].append(MediaRow[i])});
 })();
